032d42154afadccd726bbea56a992c3cb49eed44
github username <github email address>
2021-04-18 21:25:36 -0400

    when clicking on button, it changes the dog behaved status value

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ fetch(`http://localhost:3000/pups`)
 .then(data => {
     displayDogNames(data);
     addListenerToDogSpans(data)
+    
 })
 
 function displayDogNames(data){
@@ -26,10 +27,11 @@ function addListenerToDogSpans(data){
         const dogInfoDiv = document.getElementById('dog-info');
         const dogName = data[i].name;
         const dogImg = data[i].image;
-        const isDogGood = data[i].isGoodDog;
+        let isDogGood = data[i].isGoodDog;
+        const dogId = data[i].id;
 
         arrayOfDogSpans[i].addEventListener('click', (e) => {
-            
+            e.stopPropagation()
             e.preventDefault();
             dogInfoDiv.innerHTML = '';
 
@@ -40,15 +42,50 @@ function addListenerToDogSpans(data){
             createH2.innerText = dogName;
 
             const createButton = document.createElement('button');
+            createButton.setAttribute('id', 'behaved-status')
             if (isDogGood === true){
+                createButton.classList.add('good-dog');
                 createButton.innerText = `Good Dog!`
             } else if (isDogGood === false){
+                createButton.classList.add('bad-dog');
                 createButton.innerText = `Bad Dog!`
             }
             
             dogInfoDiv.appendChild(createImg);
             dogInfoDiv.appendChild(createH2);
             dogInfoDiv.appendChild(createButton);
+            
+            changeStatus(dogId, dogName, dogImg, isDogGood)
+            
         })
     }
+
+   
+}
+
+//STEP 4: TOGGLE GOOD DOG
+function changeStatus(dogId, dogName, dogImg, isDogGood){
+    const dogButton = document.getElementById('behaved-status');
+    dogButton.addEventListener('click', e => {
+        e.preventDefault();
+        if (isDogGood === true){
+            dogButton.innerText = 'Bad Dog!'
+            isDogGood = false;
+        } else if (isDogGood === false){
+            dogButton.innerText = 'Good Dog!'
+            isDogGood = true;
+        }
+        
+        fetch(`http://localhost:3000/pups/${dogId}`, {
+            method: "PATCH",
+            headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                isGoodDog: isDogGood
+            }) 
+        })
+        
+    })
 }
